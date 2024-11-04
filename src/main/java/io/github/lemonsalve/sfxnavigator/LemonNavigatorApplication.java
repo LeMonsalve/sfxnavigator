8e0a91810266f16e945fc1d59ca31aef093f0377
Juan José Monsalve Orozco <devlemonsalve@gmail.com>
2024-11-04 11:22:58 -0500

    Initial commit

---
+++ src/main/java/io/github/lemonsalve/sfxnavigator/LemonNavigatorApplication.java
@@ -0,0 +1,13 @@
+package io.github.lemonsalve.sfxnavigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemonNavigatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemonNavigatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/lemonsalve/sfxnavigator/LemonNavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lemonsalve.sfxnavigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemonNavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
